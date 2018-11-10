d8b6f0424281ddb8a94afa5bc163e8c5a0183e7c
Jonathan Stevanka <jonathan.stevanka34@stclairconnect.ca>
2018-11-10 17:26:18 -0500

    #41 implmented enchancement to registerScreen
    -created back button
    -clearbutton is functional
    -back button is functional

--- src/Screens/registerScreen.java
+++ src/Screens/registerScreen.java
@@ -3,6 +3,7 @@ package Screens;
 import javax.swing.JComboBox;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -31,7 +32,9 @@ public registerScreen(){
 			pane.setAlignment(Pos.CENTER);
 			pane.setHgap(5);
 			pane.setVgap(5);
-			
+			pane.setAlignment(Pos.CENTER);
+			//create an hbox to store the back,clear,register buttons
+			HBox row = new HBox();
 			//create the content for the gridpane
 			//create the list for the combo box
 			String[] comboContent = {"Clerk","Manager","Store-Owner"};
@@ -56,13 +59,16 @@ public registerScreen(){
 			//create a combo box to store the position's
 			ComboBox combo = new ComboBox();
 			combo.setStyle("-fx-font-family: Quicksand");
-			combo.getItems().addAll("Clerk","Manager");
+			combo.getItems().addAll("Clerk");
 			combo.getSelectionModel().select(0);
 			//create a label for the password
 			Label passwordLabel = new Label("Password:");
 			passwordLabel.setStyle("-fx-font-family: Quicksand");
 			//create a passwordfield for the password
 			PasswordField passWord = new PasswordField();
+			//create a button to traverse back to the login page
+			Button back = new Button("Back");
+			back.setStyle("-fx-border-color: B82F33; -fx-font-family: Quicksand; -fx-font-size: 12pt");
 			//create a button to reigster the user
 			Button register = new Button("Register");
 			register.setStyle("-fx-border-color: B82F33; -fx-font-family: Quicksand; -fx-font-size: 12pt");
@@ -84,11 +90,28 @@ public registerScreen(){
 			pane.add(passwordLabel, 0, 4);
 			pane.add(passWord, 1, 4,3,1);
 			
-			pane.add(clearInfo, 0, 6);
-			pane.add(register, 3, 6);
+			//set an onclick listiner for the backbutton
+			back.setOnAction(e->{
+				new logInScreen();
+			});
+			//set an onclick listener for the register button
+			register.setOnAction(e->{
+				//this is where the database query will be implemented
+			});
+			//set an onclick listener for the clear button
+			clearInfo.setOnAction(e->{
+				fname.setText("");
+				lname.setText("");
+				userName.setText("");
+				passWord.setText("");
+			});
+			//add to hbox and set to bottom
+			row.getChildren().addAll(back,clearInfo,register);
+			row.setSpacing(30);
+			row.setPadding(new Insets(0, 0, 250, 410));
 			
 			borderpane.setCenter(pane);
-
+			borderpane.setBottom(row);
 			Scene scene = new Scene(borderpane,1064,762);
 			stage.setScene(scene);
 			stage.show();
