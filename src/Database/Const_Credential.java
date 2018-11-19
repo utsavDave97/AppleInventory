6cc8d089eeba35dbdc7a357ae0e7c97c640bd0b1
JianqinWang01 <38891924+JianqinWang01@users.noreply.github.com>
2018-11-19 08:26:20 -0500

    Delete Const_Credential.java

--- src/Database/Const_Credential.java
+++
@@ -1,8 +0,0 @@
-package Database;
-
-public class Const_Credential {
-	public static final String DB_USER = "pma";
-	public static final String DB_NAME = "AppleInventory";
-	public static final String DB_PASS = "pma";
-	
-}
