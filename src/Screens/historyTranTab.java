350d9c04e816adc978b3e6aa4dcb1a3858d8a4b2
Utsav Dave <43574230+utsavDave97@users.noreply.github.com>
2018-11-19 09:57:13 -0500

    Update historyTranTab.java

--- src/Screens/historyTranTab.java
+++ src/Screens/historyTranTab.java
@@ -2,154 +2,201 @@ package Screens;
 
 import java.time.LocalDate;
 
-import Screens.TableViewItems.SaleHisItem;
+import Screens.TableViewItems.SaleItem;
+import Screens.TableViewItems.SaleRecord;
+//import Screens.currentranTab.SaleItem;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-/**
- * @description:Display the transactions made in that chosen day , transaction information
- *              can be deleted
- * @author jianqin Wang
- *
- */
-public class historyTranTab extends BorderPane{
-	//Define constructor 
-	 public historyTranTab() {
-		 //declare observableList to hold data for table View
-		 final ObservableList<SaleHisItem> dataHis =FXCollections.observableArrayList();
-		 /*******************************************************************************
-		  *            Top Search Choice                                                *
-		  ******************************************************************************/
-            //define datePicker to choose time
-			final DatePicker datePicker = new DatePicker();
+import javafx.scene.text.Text;
+
+public class searchTranTab extends BorderPane{
+	public searchTranTab() {
+		
+		this.setStyle("-fx-background-color: #DCDCDC;");
+		
+
+		//create the content for the Completed transaction
+
+		
+	       /**********************************************************************
+	        *                Top choose and add sale Item                                  *
+	        ***********************************************************************/    		
+	    //Declare a hbox to holding the operations
+	     HBox addItemBox=new HBox(10); 
+	     addItemBox.setPadding(new Insets(20,10,20,20));
+	     addItemBox.setStyle("-fx-border-color:gray;\n"
+	        		+ "-fx-border-width:0 0 1 0;\n"
+	        		+ "-fx-border-style:solid;");
+	   
+	     
+	     final DatePicker datePickerTo = new DatePicker();
 			//set datePicker style
-			datePicker.setMinHeight(40);
-			datePicker.setStyle("-fx-font-size:18");
+	     datePickerTo.setMinHeight(40);
+	     datePickerTo.setStyle("-fx-font-size:18");
 			//set event for date picker
-		    datePicker.setOnAction(new EventHandler() {
+	     datePickerTo.setOnAction(new EventHandler() {
 		    @Override
 			public void handle(Event event) {
-		    	LocalDate date = datePicker.getValue();
-		    	dataHis.add(new SaleHisItem(date.toString(), "Jianqin Wang", "1.3","50.2"));
+		    	LocalDate date = datePickerTo.getValue();
+		    	
 		         System.err.println("Selected date: " + date);
 			}
 		 });
-		 //Add search button   
-		 Button searchButton=new Button("Search");
-		 searchButton.setMinHeight(30);
-		 //Declare HBox to contain datePicker and button
-		 HBox searchHbox=new HBox();
-		 searchHbox.setSpacing(50);
-		 searchHbox.setAlignment(Pos.CENTER);
-		 searchHbox.setMinWidth(700);
-		 searchHbox.setPadding(new Insets(30,0,0,30));
-		 //add botton line to seperate underneath content
-		 searchHbox.setStyle("-fx-border-color:gray;\n" + 
-		 		             "-fx-border-width:0 0 2 0;\n" + 
-		 		             "-fx-border-style:solid;\n"
-		 		             + "-fx-padding:20 0 20 0");
-		 searchHbox.getChildren().addAll(datePicker,searchButton);
-		 /*******************************************************************************
-		  *            Center Table View  Content                                       *
-		  ******************************************************************************/
-		   //Define a table
-		   TableView<SaleHisItem> hisTable=new TableView<SaleHisItem>();
-		   dataHis.add(new SaleHisItem("2018-3-2", "Jianqin Wang", "1.3","50.2"));
-		    hisTable.setEditable(true);
-		  
-		    //Define First Column title and cell factory
-	        TableColumn timeCol = new TableColumn("Sold Time");
-	        timeCol.setMinWidth(200);
-	        timeCol.setCellValueFactory(
-	        new PropertyValueFactory<SaleHisItem, String>("saleTime"));
-	        
-	        TableColumn operatorCol = new TableColumn("Clerk");
-	        operatorCol.setMinWidth(150);
-	        operatorCol.setCellValueFactory(
-	                new PropertyValueFactory<SaleHisItem, String>("operater"));
-	        
-	        TableColumn taxCol = new TableColumn("Tax");
-	        taxCol.setMinWidth(100);
-	        taxCol.setCellValueFactory(
-	                new PropertyValueFactory<SaleHisItem, String>("tax"));
-	        
-	        TableColumn totAmountCol = new TableColumn("Total Amount");
-	        totAmountCol.setMinWidth(150);
-	        totAmountCol.setCellValueFactory(
-	         new PropertyValueFactory<SaleHisItem, String>("totAmount"));
-	       
-
-	        TableColumn checkCol = new TableColumn("Select");
-	        checkCol.setMinWidth(100);
-	        checkCol.setCellValueFactory(
-	                new PropertyValueFactory<SaleHisItem, String>("checkDel"));
-	       
-		    hisTable.getColumns().addAll(checkCol,timeCol,operatorCol,taxCol,totAmountCol);
-		 
-		    hisTable.setItems(dataHis);
-	        hisTable.setMaxWidth(750);
-	        hisTable.setMaxHeight(300);
-	        hisTable.setStyle("-fx-font-size: 16;");
-	        /*******************************************************************************
-			 *           Bottom Delete Button                                                    *
-		     ******************************************************************************/
-	        
-	        //create the delete button
-			Button delete = new Button("Delete");
-			//set the styling for the button
-			delete.setStyle("-fx-border-color: B82F33;"
-						 + "-fx-font-family: Quicksand;"
-						 + "-fx-font-size: 14pt;");
-			
-			
-							
-			 final HBox hbox = new HBox();
-			
-			 hbox.setPadding(new Insets(10, 0, 100,300));
-			    
-			 hbox.getChildren().add(delete);   
+	     
+	     
+	     //declare a label
+	     Label toLabel=new Label("To:");
+	     //set the name font
+	     toLabel.setStyle("-fx-font-family: Quicksand;"
+	 				  + "-fx-font-size: 12pt;");
+	    
+	   
+	     
+	     final DatePicker datePickerEnd = new DatePicker();
+			//set datePicker style
+	     datePickerEnd.setMinHeight(40);
+	     datePickerEnd.setStyle("-fx-font-size:18");
+			//set event for date picker
+	     datePickerEnd.setOnAction(new EventHandler() {
+		    @Override
+			public void handle(Event event) {
+		    	LocalDate date = datePickerTo.getValue();
 		    
-			 VBox centerVbox=new VBox();
-			 centerVbox.setSpacing(20);
-			 centerVbox.getChildren().addAll(hisTable,hbox);
-	        
-	        
-		 
-		 this.setTop(searchHbox);
-		 this.setCenter(centerVbox);
-		 
-       /*******************************************************************************
-			 *           Delete Function                       v                     *
-	   ******************************************************************************/
-	   delete.setOnAction(e->{
-		  //Loop to find which checkbox is selected
-		  for (SaleHisItem saleHisItem : dataHis) {
-			if(saleHisItem.getCheckDel().isSelected()) {
-				Platform.runLater(() -> {dataHis.remove(saleHisItem);});
+		         System.err.println("Selected date: " + date);
 			}
-		} 
-		   
-		   
-		   
-	   });     	 
+		 });
+	     
+	     
+	     
+	     
+	     
+	     
+	      //add a button to add more apple items
+	      Button searchButton=new Button("Search");
+	      searchButton.setStyle("-fx-border-color: B82F33;"
+						 + "-fx-font-family: Quicksand;"
+						 + "-fx-font-size: 12pt;");
+	      addItemBox.getChildren().addAll(datePickerTo,toLabel,datePickerEnd,searchButton);
+	         
+	     
 
-		 
-	 }
+
+
+/**********************************************************************
+ *                Table List Content                                  *
+ ***********************************************************************/    
+TableView<SaleRecord> table = new TableView<SaleRecord>();
+ final ObservableList<SaleRecord> data =
+        FXCollections.observableArrayList(
+        new SaleRecord("2008", "dave", "0.3","6"));
+      
+       
+table.setEditable(true);
+
+
+TableColumn timeCol = new TableColumn("Sale Time");
+timeCol.setMinWidth(200);
+timeCol.setCellValueFactory(
+        new PropertyValueFactory<SaleRecord, String>("saleTime"));
+
+TableColumn nameCol = new TableColumn("Clerk");
+
+nameCol.setCellValueFactory(
+        new PropertyValueFactory<SaleItem, String>("name"));
+nameCol.setMinWidth(200);
+
+
+TableColumn taxCol = new TableColumn("Tax");
+
+taxCol.setCellValueFactory(
+        new PropertyValueFactory<SaleItem, String>("tax"));
+
+taxCol.setMinWidth(150);
+
+
+TableColumn totCol = new TableColumn("Total Price");
+
+
+totCol.setCellValueFactory(
+        new PropertyValueFactory<SaleItem, String>("total"));
+totCol.setMinWidth(200);
+
+setMinHeight(150);
+  
+table.getColumns().addAll(timeCol,nameCol,taxCol,totCol);
+table.setItems(data);
+table.setMaxWidth(750);
+table.setStyle("-fx-font-size: 16;");
+
+/**********************************************************************
+ *                Bottom tax and totalAmount                           *
+ ***********************************************************************/  
+
+
+final VBox vbox = new VBox();
+vbox.setSpacing(5);
+vbox.setPadding(new Insets(30, 10, 0, 10));
+
+vbox.getChildren().addAll(table);
+
+//create the submit button
+Button clear = new Button("Clear");
+//create the register styling
+clear.setStyle("-fx-border-color:B82F33;"
+			  + "-fx-font-family: Quicksand;"
+			  + "-fx-font-size: 12pt;");
+				
+ final HBox hbox = new HBox();
+ hbox.setSpacing(200);
+ hbox.setPadding(new Insets(60, 0, 100, 300));
+    
+ hbox.getChildren().addAll(clear);
+//Set content to GridPane
+
+
+this.setTop(addItemBox);
+this.setCenter(vbox);
+this.setBottom(hbox);
+//create the scene
+
+
+/**************************************************************************************
+*         add  button event
+*************************************************************************************/
+
+//add search button action
+
+  searchButton.setOnAction(e->{
+	  
+  });
+  
+  //add clear button
+  
+  clear.setOnAction(e->{
+		for(SaleRecord saleRecord:data) {
+    		Platform.runLater(() -> {data.remove(saleRecord);});
+    	}
+  });
+
+
+}
 
 
 }
