ed2544506c6c673271300303f6dcfbbc4d99dcdf
Jonathan Stevanka <jonathan.stevanka34@stclairconnect.ca>
2018-12-02 14:02:36 -0500

    fixed sql error

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -31,7 +31,7 @@ public class DBConnection
 				Class.forName("com.mysql.jdbc.Driver");
 				dbConnection = 
 				DriverManager//192.168.64.2
-				.getConnection("jdbc:mysql://127.0.0.1/"+
+				.getConnection("jdbc:mysql://192.168.64.2/"+
 				 Const_Credential.DB_NAME + "?useSSL=false",
 				 Const_Credential.DB_USER, Const_Credential.DB_PASS);
 				System.out.println("Successfully Created Connection");
